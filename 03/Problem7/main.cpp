493c1f46fbdba15b3464b47044788720aab69fc7
Viacheslav Konstantinov <vyacheslav.konstantinov@phystech.edu>
2019-11-20 15:24:47 +0300

    Корректировка 1 задачи 7_1
    1) Для выполнения "Правила 5-и" в ArmyLine явно удалены конструкторы копирования, перемещения, оператор присваивания копированием и оператор присваивания перемещением;
    2) ArmyLine(const int height) ->  explicit ArmyLine(const int key);
    3) В структуре Node height заменён на key;
    4) Node(int key) -> explicit Node(int key);
    5) Node(Node* node) убрал. В предыдущей версии я его прописывал явно, поскольку не был уверен в поведении стандартного конструктора копирования.

--- 03/Problem7/main.cpp
+++ 03/Problem7/main.cpp
@@ -1,258 +1,97 @@
 /*
-7_1. Солдаты ДД
-
-Ограничение времени	1 секунда
-Ограничение памяти	500Mb
-
+6_1. Обход дерева в порядке pre-order
+Ограничение времени	0.2 секунды
+Ограничение памяти	64Mb
 Ввод	стандартный ввод или input.txt
 Вывод	стандартный вывод или output.txt
 
-В одной военной части решили построить в одну шеренгу по росту. 
-Т.к. часть была далеко не образцовая, то солдаты часто приходили не вовремя, 
-а то их и вовсе приходилось выгонять из шеренги за плохо начищенные сапоги. 
-Однако солдаты в процессе прихода и ухода должны были всегда быть выстроены по росту – 
-сначала самые высокие, а в конце – самые низкие. За расстановку солдат отвечал прапорщик, 
-который заметил интересную особенность – все солдаты в части разного роста.
+Дано число N < 106 и последовательность целых чисел из [-231..231] длиной N. 
+Требуется построить бинарное дерево, заданное наивным порядком вставки. 
+Т.е., при добавлении очередного числа K в дерево с корнем root, 
+если root→Key ≤ K, то узел K добавляется в правое поддерево root; иначе в левое 
+поддерево root. Выведите элементы в порядке pre-order (сверху вниз).
 
-Ваша задача состоит в том, чтобы помочь прапорщику правильно расставлять солдат, а 
-именно для каждого приходящего солдата указывать, перед каким солдатом в строе он должен 
-становится. Требуемая скорость выполнения команды - O(log n) в среднем.
-
-В реализации используйте декартовы деревья.
-
-Формат ввода
-Первая строка содержит число N – количество команд (1 ≤ N ≤ 90 000).
-
-В каждой следующей строке содержится описание команды: число 1 и X если солдат приходит 
-в строй (X – рост солдата, натуральное число до 100 000 включительно) и число 2 и Y если 
-солдата, стоящим в строе на месте Y надо удалить из строя. Солдаты в строе нумеруются с нуля.
-
-Формат вывода
-На каждую команду 1 (добавление в строй) вы должны выводить число K – номер позиции, на 
-которую должен встать этот солдат (все стоящие за ним двигаются назад).
-
+Рекурсия запрещена.
 */
+
 #include <iostream>
-#include <bits/stdc++.h>
+#include <algorithm>
 #include <stack>
 
-using namespace std;
+using KeyCallBack = void (*)(int);
 
-class ArmyLine
-{
+class BinaryTree
+{   
     public:
-        ArmyLine()
-            : root_(nullptr) {}
-        ArmyLine(const int height)
-            : root_(new Node(height)) {}
-        ~ArmyLine();
+        explicit BinaryTree(const int keyOfRoot)
+            : size_(1), root_(new Node(keyOfRoot)) {}
+        
+        ~BinaryTree();
+        BinaryTree(const BinaryTree&) = delete;
+        BinaryTree(BinaryTree&& other) = delete;
+        BinaryTree& operator=(const BinaryTree&) = delete;
+        BinaryTree& operator=(BinaryTree&&) = delete;
 
-        int getIdxOfHeight(int height);
-        
-        int add(int height);
-        int get(int idx);
-        void remove(int idxToRemove);
-        
-        void print();
-    
+        void Add(const int key);
+        void PrintTreePostOrder();
+        void PostOrder(KeyCallBack visit);
+
     private:
         struct Node
         {
-            int height;
-            int priority;
-            int numberOfSubtrees; // количество поддеревьев в узле, включая сам узел
-            Node* left;
-            Node* right;
-            
-            Node(int key)
-                : height(key), left(nullptr), right(nullptr), priority(rand() % 1000000), numberOfSubtrees(1) {};
+            int key;
+            Node* left = nullptr;
+            Node* right = nullptr;
             Node(Node* node)
-                : height(node->height), left(node->left), right(node->right), priority(node->priority), numberOfSubtrees(node->numberOfSubtrees) {};
-        };
+                : key(node->key), left(node->left), right(node->right) {};
+            explicit Node(const int key)
+                : key(key),  left(nullptr), right(nullptr) {};
 
+            bool operator==(const Node& other) const
+            {
+                return (key == other.key && left == other.left && right == other.right);
+            }
+        };
+        
+        int size_;
         Node* root_;
 
-        int getNumberOfSubtrees_(Node* node);
-        void updateNumberOfSubtrees_(Node* node);
-        int getKIndex_(Node* node, int k);
-
-        Node* merge_(Node* T1, Node* T2);
-        void split_(Node* T, int key, Node*& T1, Node*& T2);
-        void RemoveByKey_(int height);
-
-        // DoPostOrder_ используется в методе печати и деструкторе
-        using keyCallback = void (*)(int);
-        using nodeCallback = void (*)(Node*);
-        void DoPostOrder_(Node*& node, keyCallback doWithKey, nodeCallback doWithNodes);
+        using NodeCallBack = void (*)(Node*);
+        // DoPostOrder_ используется в методе печати, деструкторе
+        // и при добавлении пользовательского образотчика
+        void DoPostOrder_(NodeCallBack visitNode, KeyCallBack visitKey);
 };
 
-//метод получения количества поддеревьев в узле
-int ArmyLine::getNumberOfSubtrees_(Node* node) 
+BinaryTree::~BinaryTree()
 {
-    return node ? node->numberOfSubtrees: 0;
+    //обойдем дерево и удалим каждый из узлов
+    DoPostOrder_([](Node* node)->void{delete node; }, [](int key)->void{return; });
+}
+void BinaryTree::PrintTreePostOrder()
+{
+    //обойдем дерево и напечататем каждый из узлов
+    //можно было бы обойтись и без этого метода,
+    //если в деструкторе перед удалением узла печатать значение ключа
+    DoPostOrder_([](Node* node)->void{return; }, [](int key)->void{std::cout << key << " "; });
 }
 
-//метод обновления количества значения количества поддеревьев в узлах
-void ArmyLine::updateNumberOfSubtrees_(Node* node)
+//метод для добавления пользовательского обработчика 
+// KeyCallBack = void (*)(int);
+void BinaryTree::PostOrder(KeyCallBack visit)
 {
-    if(node)
-        node->numberOfSubtrees = 1 +  getNumberOfSubtrees_(node->left) + getNumberOfSubtrees_(node->right);
-}
 
-//получение роста бойка на позиции k
-int ArmyLine::getKIndex_(Node* node, int k)
-    {
-    if(k < getNumberOfSubtrees_(node->right))
-        return getKIndex_(node->right, k);
-    else if (k == getNumberOfSubtrees_(node->right))
-        return node->height;
-    else
-        return getKIndex_(node->left, k - 1 - getNumberOfSubtrees_(node->right));
+    DoPostOrder_([](BinaryTree::Node* node)->void{return; }, visit);
 }
-
-//стандартный merge_
-//с обновлением числа подеревьев в корне после склеивания
-ArmyLine::Node* ArmyLine::merge_(Node* T1, Node* T2)
+//метод обхода
+void BinaryTree::DoPostOrder_(NodeCallBack doWithNodes, KeyCallBack doWithKey)
 {
-    if(!T1) return T2;
-    if(!T2) return T1;
-
-    if(T1->priority > T2->priority)
-    {
-        T1->right = merge_(T1->right, T2);
-            updateNumberOfSubtrees_(T1);
-            return T1;
-        }
-    else
-    {
-        T2->left = merge_(T1, T2->left);
-        updateNumberOfSubtrees_(T2);
-            return T2;
-    }
-}
-
-//стандартный split_ декартового дерева
-//с обновлением числа подеревьев в корне после разрезания
-void ArmyLine::split_(Node* T, int key, Node*& T1, Node*& T2)
-{
-    if(!T)
-    {
-        T1 = T2 = nullptr;
-        return;
-    }
-
-    if(T->height < key)
-    {
-        split_(T->right, key, T->right, T2);
-        T1 = T;
-    }
-    else
-    {
-        split_(T->left, key, T1, T->left);
-        T2 = T;
-    }
-    updateNumberOfSubtrees_(T);
-}
-
-//удаление бойца с ростом height
-void ArmyLine::RemoveByKey_(int height)
-{
-    Node *T1, *T2, *T3, *T4;
-    
-    split_(root_, height, T1, T2);
-    split_(T2, height+1, T3, T4);
-
-    root_ = merge_(T1, T4);
-
-    delete T3;
-}
-
-//получение индекса - номера в строю по росту
-int ArmyLine::getIdxOfHeight(int height)
-{
-    if(!root_)
-        return 0;
-    int currentIdx(0), leftSubTrees(0), rightSubtrees(0);
-            
-    currentIdx = root_->numberOfSubtrees;
-
-    if(root_->left) 
-        leftSubTrees = root_->left->numberOfSubtrees;
-
-    currentIdx = currentIdx - leftSubTrees;
-            
-    if (root_->height == height)
-        return currentIdx - 1;
-    
-    Node* currentNode = root_;
-    
-    while(true)
-    {
-        if(currentNode->height > height)
-        {
-            currentNode = currentNode->left;
-            
-            if(currentNode->right) 
-                rightSubtrees = currentNode->right->numberOfSubtrees;
-            else
-                rightSubtrees = 0;
-            
-            currentIdx = currentIdx + rightSubtrees + 1;
-        }
-                
-        if(currentNode->height < height)
-        {
-            currentNode = currentNode->right;
-            
-            if(currentNode->left)  
-                leftSubTrees = currentNode->left->numberOfSubtrees;
-            else 
-                leftSubTrees = 0;
-            
-            currentIdx = currentIdx - leftSubTrees - 1;
-        }
-                
-        if(currentNode->height == height)
-            return currentIdx - 1;
-    }
-}
-
-//добавление бойца с ростом height
-int ArmyLine::add(int height)
-{
-    Node *T1, *T2;
-
-    split_(root_, height, T1, T2);
-    Node* nodeToAdd = new Node(height);
-
-    root_ = merge_(merge_(T1, nodeToAdd), T2);
-
-    return getIdxOfHeight(height);
-}
-
-//удаление из строя бойца на позиции idxToRemove
-void ArmyLine::remove(int idxToRemove)
-{
-    RemoveByKey_(getKIndex_(root_, idxToRemove));
-}
-
-//получить рост по номеру в строю
-//дополнительный к getKIndex_ нужен, чтобы не передавать ссылку на root_
-int ArmyLine::get(int idx)
-{
-    return getKIndex_(root_, idx);
-}
-
-//метод обхода, необходимый для удаления и печати
-void ArmyLine::DoPostOrder_(Node*& node, keyCallback doWithKey, nodeCallback doWithNodes)
-{
-    stack<Node*> nodeStack;
+    std::stack<Node*> nodeStack;
     Node* currentNode;
-    currentNode = node;
+    currentNode = root_;
     Node* lastVisitedNode = nullptr;
     Node* topOfStack = nullptr;
  
-    while ((nodeStack.size() > 0) or currentNode) 
+    while ((nodeStack.size() > 0) or currentNode)
     {
         if (currentNode) 
         {
@@ -267,50 +106,63 @@ void ArmyLine::DoPostOrder_(Node*& node, keyCallback doWithKey, nodeCallback doW
             else 
             {
                 nodeStack.pop();
-                doWithKey(topOfStack->height);
                 lastVisitedNode = topOfStack;
+                doWithKey(topOfStack->key);
                 doWithNodes(topOfStack);
             }
         }
     }
 }
 
-// печать дерева
-void ArmyLine::print()
+void BinaryTree::Add(const int key)
 {
-    DoPostOrder_(root_, [](int x)->void {cout << x << " ";}, [](Node* node) ->void {return;});
-    cout << endl;
-    return;
-}
+    size_ += 1;
+    
+    Node* nodeToAdd = new Node(key);
+    Node* currentNode = root_;
 
-//удаление дерева
-ArmyLine::~ArmyLine()
-{
-    //обойдем дерево и удалим каждый из узлов
-    DoPostOrder_(root_, [](int x)->void{return;}, [](Node* node)->void{delete node; });
+    while(currentNode != nullptr)
+    {
+        if (key > currentNode->key)
+        {
+            if (currentNode->right)
+                currentNode = currentNode->right;
+            else
+            {
+                currentNode->right = nodeToAdd;
+                break;
+            }
+        }
+        else
+        {
+            if(currentNode->left)
+                currentNode = currentNode->left;
+            else
+            {
+                currentNode->left = nodeToAdd;
+                break;
+            }
+        }
+    }
 }
 
 int main()
 {
-    
-    int numberOfComands;
-    cin >> numberOfComands;
+   size_t treeSize;
+   int keyBuffer;
+   
+   std::cin >> treeSize;
 
-    ArmyLine myLine;
-    int typeOfComand;
-    int comandArgument;
+   std::cin >> keyBuffer;
+   BinaryTree myTree(keyBuffer);
+   
+   for (size_t i = 1; i < treeSize; ++i)
+   {
+       std::cin >> keyBuffer;
+       myTree.Add(keyBuffer);
+   }
+   
+   myTree.PrintTreePostOrder();
 
-    for(int i = 0; i< numberOfComands; ++i)
-    {
-        cin >> typeOfComand >> comandArgument;
-        if(typeOfComand == 1)
-            cout << myLine.add(comandArgument) << endl;
-                        
-        else if (typeOfComand == 2)
-            myLine.remove(comandArgument);
-    }
-    
-    //myLine.print();
-
-    return 0;
+   return 0;
 }
