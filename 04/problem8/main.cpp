1f829b480c680498e14bb7722798268c943aeb6d
Viacheslav Konstantinov <vyacheslav.konstantinov@phystech.edu>
2019-12-01 14:41:47 +0300

    Корректировка по задаче 8
    1) В таблице std::vector<HashTableNode> table_ хранятся не указатели, а сами объекты. Добавлено поле isEmpty в struct HashTableNode показывает, пуст ли ключ key экземпляра или нет. 
    2) Класс HashTable, функции hashFunc1 и hashFunc2 сделаны шаблонными. Чтобы в hashFunc1 сохранить применение метода Горнера, ключ key предварительно сериализуется в массим байтов-символов функцией convertToBytes.

--- 04/problem8/main.cpp
+++ 04/problem8/main.cpp
@@ -42,122 +42,148 @@ FAIL иначе.
 
 */
 
-
-
 #include <iostream>
 #include <string>
 #include <vector>
+#include <array>
+#include <memory>
+#include <type_traits>
+#include <iomanip>
 
 #define HASH_TABLE_DEFAULT_BUFFER_SIZE 8
 #define REHASH_RATIO 0.75
 #define HASH_P 31
 
-int hashFunc1(const std::string &key, int size) 
+
+using byte = unsigned char;
+
+//сериализация объекта произвольного типа T
+template <typename T> 
+std::array< byte, sizeof(T)>  convertToBytes( const T& object )
 {
-    int hash = 0;
-    for (int i = 0; i < key.size(); i++)
-        hash = (HASH_P * hash) + key[i];
+    std::array<byte, sizeof(T)> bytes ;
+
+    const byte* begin = reinterpret_cast< const byte* >( std::addressof(object) ) ;
+    const byte* end = begin + sizeof(T) ;
+    std::copy(begin, end, std::begin(bytes)) ;
+
+    return bytes;
+}
+
+template <typename T> 
+size_t hashFunc1(const T &key, size_t size) 
+{
+    size_t hash = 0;
+    std::array<byte, sizeof(T)>keySerialized = convertToBytes(key);
+
+    for (auto item = keySerialized.begin(); item != keySerialized.end(); ++item)
+        hash = (HASH_P * hash) + (size_t)*item;
     return (hash & 0x7fffffff) % size;
 }
 
-int hashFunc2(const std::string &key) 
+template <typename T>
+size_t hashFunc2(const T &key) 
 {
-    return (int)key[0]*2 + 1;
+    std::array<byte, sizeof(T)>keySerialized = convertToBytes(key);
+
+    return (size_t)keySerialized.at(0)*2 + 1;
 }
 
+template <typename T>
 class HashTable 
 {
     public:
         HashTable()
-             : table_(HASH_TABLE_DEFAULT_BUFFER_SIZE, nullptr), 
-                itemCounter_ (0), 
-                    tableSize_(HASH_TABLE_DEFAULT_BUFFER_SIZE) {}
-        ~HashTable();
-        HashTable(const HashTable&) = delete;
-        HashTable(HashTable&&) = delete;
-        HashTable& operator=(const HashTable&) = delete;
-        HashTable& operator=(HashTable&&) = delete;
+             : itemCounter_ (0), table_(HASH_TABLE_DEFAULT_BUFFER_SIZE, HashTableNode()) {}
 
-        bool Has(const std::string& key) const;
-        bool Add(const std::string& key);
-        bool Remove(const std::string& key);
+        bool Has(const T& key) const;
+        bool Add(const T& key);
+        bool Remove(const T& key);
         void Print();
 
     private:
         struct HashTableNode 
         {
-            std::string key;
+            T key;
             bool isDeleted;
-            explicit HashTableNode(const std::string& key_) 
-            : key(key_), isDeleted(false) {}
-        };
-        std::vector<HashTableNode*> table_;
+            bool isEmpty;
+            explicit HashTableNode(const T& key_) 
+            : key(key_), isDeleted(false), isEmpty(false) {}
+            HashTableNode() 
+            : isDeleted(false), isEmpty(true) {}
 
-        int itemCounter_ ;
-        int tableSize_;
+            bool operator==(const HashTableNode& other) const
+            {
+                return (key == other.key && isDeleted == other.isDeleted && isEmpty == other.isEmpty);
+            }
+        };
+
+        
+
+        std::vector<HashTableNode> table_;
+        size_t tableSize_ = HASH_TABLE_DEFAULT_BUFFER_SIZE;
+        size_t itemCounter_ ;
+
         void Rehash_();
 };
 
-HashTable::~HashTable() 
+template <typename T>
+void HashTable<T>::Print()
 {
-    for (HashTableNode* head: table_)
-        delete head;
-}
-
-void HashTable::Print()
-{
-    for (int i = 0; i < tableSize_ ; ++i)
+    for (size_t i = 0; i < tableSize_ ; ++i)
     {
-        if (table_[i] && !table_[i]->isDeleted)
-            std::cout << "idx = " << i << ", key = " << table_[i]->key << std::endl;
+        if (!table_[i].isEmpty)
+            std::cout << "idx = " << i << ", key = " << table_[i].key << std::endl;
     }
+        
 }
-void HashTable::Rehash_()
+
+template <typename T>
+void HashTable<T>::Rehash_()
 {
-    int newSize = tableSize_ * 2;
-    std::vector<HashTableNode*> newTable(newSize, nullptr);
+    size_t newSize = tableSize_ * 2;
+    std::vector<HashTableNode> newTable(newSize, HashTableNode());
 
-    for(int i = 0; i < tableSize_; i++) 
+    for(size_t i = 0; i < tableSize_; ++i) 
     {
-        if(table_[i] != nullptr && !table_[i]->isDeleted) 
+        if(!table_[i].isEmpty && !table_[i].isDeleted) 
         {
-            int h1 = hashFunc1(table_[i]->key, newSize),
-                h2 = hashFunc2(table_[i]->key);
+            size_t  h1 = hashFunc1(table_[i].key, newSize),
+                    h2 = hashFunc2(table_[i].key);
 
-                int j = 1;
-                int currentHash = h1;
-                while(newTable[currentHash] != nullptr && j < newSize) 
+                size_t j = 1;
+                size_t currentHash = h1;
+                while(!newTable[currentHash].isEmpty && j < newSize) 
                 {
                     currentHash = (h1 + j*h2) % newSize;
                     j++;
                 }
-                newTable[currentHash] = table_[i];
+                newTable[currentHash] = HashTableNode(table_[i].key);
         }
-        else
-            delete table_[i];
         
-        table_[i] = nullptr;
+        table_[i].isEmpty = false;
     }
     table_ = newTable;
     tableSize_ = newSize;
 }
 
-bool HashTable::Add(const std::string& key) 
+template <typename T>
+bool HashTable<T>::Add(const T& key) 
 {
     if( ((double) itemCounter_  / (double) tableSize_) >= REHASH_RATIO)
-        Rehash_();
+        HashTable<T>::Rehash_();
 
     int h1 = hashFunc1(key, tableSize_), 
         h2 = hashFunc2(key);
 
     int i = 1, idxFirstDeleted = -1, currentHash = h1;
 
-    while(table_[currentHash] != nullptr && i < tableSize_) 
+    while(!table_[currentHash].isEmpty && i < tableSize_) 
     {
-        if(table_[currentHash]->key == key && !table_[currentHash]->isDeleted)
+        if(table_[currentHash].key == key && !table_[currentHash].isDeleted)
             return false;
 
-        if(table_[currentHash]->isDeleted && idxFirstDeleted < 0)
+        if(table_[currentHash].isDeleted && idxFirstDeleted < 0)
             idxFirstDeleted = currentHash;
 
         currentHash = (h1 + i*h2) % tableSize_;
@@ -165,11 +191,11 @@ bool HashTable::Add(const std::string& key)
     }
 
     if(idxFirstDeleted < 0)
-        table_[currentHash] = new HashTableNode(key);
+        table_[currentHash] = HashTableNode(key);
     else
     {
-        table_[idxFirstDeleted]->key = key;
-        table_[idxFirstDeleted]->isDeleted = false;
+        table_[idxFirstDeleted].key = key;
+        table_[idxFirstDeleted].isDeleted = false;
     }
 
     itemCounter_++;
@@ -177,16 +203,17 @@ bool HashTable::Add(const std::string& key)
     return true;
 }
 
-bool HashTable::Has(const std::string& key) const 
+template <typename T>
+bool HashTable<T>::Has(const T& key) const 
 {   
     int h1 = hashFunc1(key, tableSize_), 
         h2 = hashFunc2(key);
     
     int i = 1, currentHash = h1;
     
-    while(table_[currentHash] != nullptr && i < tableSize_) 
+    while(!table_[currentHash].isEmpty && i < tableSize_) 
     {
-        if(table_[currentHash]->key == key && !table_[currentHash]->isDeleted) 
+        if(table_[currentHash].key == key && !table_[currentHash].isDeleted) 
             return true;
 
         currentHash = (h1 + i*h2) % tableSize_;
@@ -196,19 +223,20 @@ bool HashTable::Has(const std::string& key) const
     return false;
 }
 
-bool HashTable::Remove(const std::string& key) 
+template <typename T>
+bool HashTable<T>::Remove(const T& key) 
 {
     int h1 = hashFunc1(key, tableSize_), 
         h2 = hashFunc2(key);
     
     int i = 1, currentHash = h1;
     
-    while(table_[currentHash] != nullptr  && i < tableSize_) 
+    while(!table_[currentHash].isEmpty && i < tableSize_) 
     {
-        if(table_[currentHash]->key == key && !table_[currentHash]->isDeleted)
+        if(table_[currentHash].key == key && !table_[currentHash].isDeleted)
         {
             itemCounter_--;
-            table_[currentHash]->isDeleted = true;
+            table_[currentHash].isDeleted = true;
                 
             return true;
         }
@@ -222,7 +250,7 @@ bool HashTable::Remove(const std::string& key)
 
 int main() 
 {
-    HashTable myTable;
+    HashTable<std::string> myTable;
 
     char command = ' ';
     std::string key("");
